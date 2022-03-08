3a9aeb94edbd813cb90942d569c1aa69dc281227
Karl Lorey <git@karllorey.com>
2022-03-08 14:49:07 +0100

    Add icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import {Link} from "gatsby"
+import {FaAngellist, FaCube, FaGithub, FaGitlab, FaGoodreads, FaInstagram, FaLinkedin, FaMedium, FaMeetup, FaProductHunt, FaResearchgate, FaTwitter} from 'react-icons/fa'
 
 export default function Footer() {
     return (
         <footer className="text-center text-sm py-10">
             <p className="">I'm Karl Lorey–Techie, Founder, and Investor living in Karlsruhe, Germany. Let's get in touch.</p>
-            <p className="lead socials">
-                <a href="https://angel.co/karllorey" target="_blank"><i className="fab fa-angellist"></i></a>
-                <a href="https://www.crunchbase.com/person/karl-lorey" target="_blank"><i className="fas fa-cube"></i></a>
-                <a href="https://github.com/lorey" target="_blank"><i className="fab fa-github"></i></a>
-                <a href="https://gitlab.com/lorey" target="_blank"><i className="fab fa-gitlab"></i></a>
-                <a href="https://www.goodreads.com/karllorey" target="_blank"><i className="fab fa-goodreads"></i></a>
-                <a href="https://www.instagram.com/karllorey" target="_blank"><i className="fab fa-instagram"></i></a>
-                <a href="https://www.linkedin.com/in/karllorey" target="_blank"><i className="fab fa-linkedin"></i></a>
-                <a href="https://medium.com/@karllorey" target="_blank"><i className="fab fa-medium"></i></a>
-                <a href="https://www.meetup.com/members/196097665/" target="_blank"><i className="fab fa-meetup"></i></a>
-                <a href="https://www.producthunt.com/@karllorey" target="_blank"><i className="fab fa-product-hunt"></i></a>
-                <a href="https://www.researchgate.net/profile/Karl_Lorey" target="_blank"><i className="fab fa-researchgate"></i></a>
-                <a href="https://twitter.com/karllorey" target="_blank"><i className="fab fa-twitter"></i></a>
+            <p className="flex justify-center gap-5 flex-wrap text-xl">
+                <a href="https://angel.co/karllorey" target="_blank"><FaAngellist className="inline" /></a>
+                <a href="https://www.crunchbase.com/person/karl-lorey" target="_blank"><FaCube className="inline" /></a>
+                <a href="https://github.com/lorey" target="_blank"><FaGithub className="inline" /></a>
+                <a href="https://gitlab.com/lorey" target="_blank"><FaGitlab className="inline" /></a>
+                <a href="https://www.goodreads.com/karllorey" target="_blank"><FaGoodreads className="inline" /></a>
+                <a href="https://www.instagram.com/karllorey" target="_blank"><FaInstagram className="inline" /></a>
+                <a href="https://www.linkedin.com/in/karllorey" target="_blank"><FaLinkedin className="inline" /></a>
+                <a href="https://medium.com/@karllorey" target="_blank"><FaMedium className="inline" /></a>
+                <a href="https://www.meetup.com/members/196097665/" target="_blank"><FaMeetup className="inline" /></a>
+                <a href="https://www.producthunt.com/@karllorey" target="_blank"><FaProductHunt className="inline" /></a>
+                <a href="https://www.researchgate.net/profile/Karl_Lorey" target="_blank"><FaResearchgate className="inline" /></a>
+                <a href="https://twitter.com/karllorey" target="_blank"><FaTwitter className="inline" /></a>
             </p>
             <p className="mt-5">
                 This site uses cookies and external services to improve your experience.<br/>
